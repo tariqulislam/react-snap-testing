eb1a3f3d242fd9722e690b650339ef3a1f153d6a
Tariqul Islam <tariqul.islam.rony@gmail.com>
2018-12-12 18:58:07 +0600

    Add Test Case with People information

--- src/components/People.spec.js
+++ src/components/People.spec.js
@@ -7,4 +7,10 @@ describe(`Test People Information in row`, () => {
       const tree = renderer.create(<People />).toJSON();
       expect(tree).toMatchSnapshot();
   });
+
+  it('renders the row with people information', () => {
+      const people = { id: 1, username: 'tariqulislam', email: 'tariqul.islam.rony@gmail.com', phone: 'phone'};
+      const tree = renderer.create(<People item={people}  />).toJSON();
+      expect(tree).toMatchSnapshot();
+  })
 });
