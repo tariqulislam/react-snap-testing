55795b31e1459be305113cdda8456fd1d4ef327d
tariqul islam <tariqul.islam.rony@gmail.com>
2018-12-15 11:08:38 +0600

    add the spanshot testing for people list

--- src/components/PeopleList.spec.js
+++ src/components/PeopleList.spec.js
@@ -18,4 +18,23 @@ describe('It Should be testing People List Component', () => {
         const tree = renderer.create(<PeopleList lstPeoples={lstPeoples} />).toJSON();
         expect(tree).toMatchSnapshot()
     });
+
+    it('Should render the 1 rows In people List', () => {
+        let peopleList = [{
+            id: 1,
+            username: "Bret",
+            email: "Sincere@april.biz",
+            phone: "1-770-736-8031 x56442"
+        },
+        {
+            id: 2,
+            username: "Lean",
+            email: "lean@april.biz",
+            phone: "1-770-736-8033 x56442"
+        }
+        ];
+
+          const tree = renderer.create(<PeopleList lstPeoples={peopleList} />).toJSON();
+          expect(tree).toMatchSnapshot()
+    });
 });
